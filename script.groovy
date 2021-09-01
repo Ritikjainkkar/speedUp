3611a25b48f448597253f7a983b72ca0513be2b3
Ritikjainkkar <ritikjainkkar@gmail.com>
2021-09-01 17:18:16 +0530

    modified after workin

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+  echo 'build the application...'
+  sh 'npm install'
+}
+
+def buildImg() {
+  echo 'build image the application...'
+  withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh 'docker build -t temporaryrk/demo:sp-2.0 .'
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push temporaryrk/demo:sp-2.0'
+  }
+}
